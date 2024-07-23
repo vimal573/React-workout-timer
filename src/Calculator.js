7abf994a7bd59c2f4cafdfae1dd1a3796c0bcc6b
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-23 15:19:45 +0530

    Setting state based on other state

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,6 @@
 import { memo, useState } from 'react';
 import clickSound from './ClickSound.m4a';
+import { useEffect } from 'react';
 
 function Calculator({ workouts, allowSound }) {
   const [number, setNumber] = useState(workouts.at(0).numExercises);
@@ -7,7 +8,17 @@ function Calculator({ workouts, allowSound }) {
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  const [duration, setDuration] = useState(0);
+
+  useEffect(
+    function () {
+      setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+    },
+    [number, sets, speed, durationBreak]
+  );
+
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
@@ -16,6 +27,13 @@ function Calculator({ workouts, allowSound }) {
     const sound = new Audio(clickSound);
     sound.play();
   };
+
+  function handleInc() {
+    setDuration(duration => Math.floor(duration) + 1);
+  }
+  function handleDec() {
+    setDuration(duration => (duration > 1 ? Math.ceil(duration) - 1 : 0));
+  }
 
   return (
     <>
@@ -66,13 +84,13 @@ function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDec}>–</button>
         <p>
           {mins < 10 && '0'}
           {mins}:{seconds < 10 && '0'}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleInc}>+</button>
       </section>
     </>
   );
